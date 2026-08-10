e954b0e46d587c345159844b07caaddf1ad26f71
felip <felip@Felipe>
2026-08-10 12:10:21 -0600

    Reconocedor de directivas listo

---
+++ src/main/java/AnalizadorLexico/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AnalizadorLexico;
+
+/**
+ *
+ * @author felip
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("+------+----------------+------------+------+---------+");
+        System.out.printf("| %-4s | %-14s | %-10s | %-4s | %-7s |%n",
+                "No.", "Lexema", "Tipo", "Fila", "Columna");
+        System.out.println("+------+----------------+------------+------+---------+");
+
+        LeerArchivo leer = new LeerArchivo();
+        leer.leerTextoEnBytes();
+    }
+}
